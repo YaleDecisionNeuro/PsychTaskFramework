fb0b83fecb4c8aa60a97ace87baeecbf786328ac
Simon Podhajsky <simon.podhajsky@gmail.com>
2017-02-14 12:37:36 -0500

    Handle phase function omission
    Experimenter might wish to omit a phase by setting its function handle
    to NaN in `s.game.responsePhaseFn` (e.g. if response is collected
    during the choice display phase). This edit makes sure that `runTrial`
    will omit the phase gracefully if that happens..

--- lib/runTrial.m
+++ lib/runTrial.m
@@ -6,17 +6,34 @@ function trialData = runTrial(trialSettings, blockSettings)
 
 % Record the properties of this trial to trialData
 trialData.trialStartTime = datevec(now);
+
+% Create convenience variables
 s = blockSettings.game;
 optionsPhase = s.optionsPhaseFn;
 responsePhase = s.responsePhaseFn;
 feedbackPhase = s.feedbackPhaseFn;
 intertrialPhase = s.intertrialPhaseFn;
 
+% 1. Display the choice for the trial
 trialData = optionsPhase(trialData, trialSettings, blockSettings);
-trialData = responsePhase(trialData, trialSettings, blockSettings);
+
+% 2. If defined, display the response-collecting phase
+if isa(responsePhase, 'function_handle')
+  trialData = responsePhase(trialData, trialSettings, blockSettings);
+end
+
+% Print choice to stdout
 disp(choiceReport(trialData, trialSettings));
-trialData = feedbackPhase(trialData, trialSettings, blockSettings);
-trialData = intertrialPhase(trialData, trialSettings, blockSettings);
+
+% 3. If defined, display the feedback phase
+if isa(feedbackPhase, 'function_handle')
+  trialData = feedbackPhase(trialData, trialSettings, blockSettings);
+end
+
+% 4. If defined, display the intertrial phase
+if isa(intertrialPhase, 'function_handle')
+  trialData = intertrialPhase(trialData, trialSettings, blockSettings);
+end
 
 trialData.trialEndTime = datevec(now);
 end
