a8cd23e18c944ff84b895adf5063a76363c1373f
Simon Podhajsky <simon.podhajsky@gmail.com>
2017-02-20 19:20:50 -0500

    Implements one possible catch trial storage (#58)

--- lib/generateTrials.m
+++ lib/generateTrials.m
@@ -2,12 +2,16 @@ function [ trialTbl ] = generateTrials(levelSettings)
 % GENERATETRIALS Generates a table of risk&ambiguity trials that contain all
 %   combinations of `levelSettings` struct subfields.
 %
-% NOTE: The generator is specific to the risk/ambiguity task. It expects that
-% the struct will contain (`stakes`, `probs`, `ambigs`) fields, and will
-% combine them in a particular way (specifically, assuming that all ambiguous
-% trials are coupled with .5 probability, and all probability trials have at
-% least one appearance with zero ambiguity). It distributes all other subfields
-% it finds randomly across thus generated trials.
+% NOTE: The generator is specific to the risk/ambiguity task. It expects that %
+% the struct will contain `stakes` and at least one of (`probs`, `ambigs`)
+% fields.  % It will also combine them in a particular way (specifically,
+% assuming that all ambiguous % trials are coupled with .5 probability, and all
+% probability trials have at % least one appearance with zero ambiguity). It
+% distributes all other subfields % it finds randomly across thus generated
+% trials.
+%
+% If you wish to omit any ambiguous trials but do not wish to delete the struct
+% field, you can also pass an empty matrix to achieve the same result.
 %
 % Generally speaking: if your task only differs slightly from the monetary R&A
 % task, you should be able to use this script with minimum alteration. If your
@@ -15,26 +19,28 @@ function [ trialTbl ] = generateTrials(levelSettings)
 
 %% 1. Generate the basics -- assuming field names contain well-defined content
 allStakes = levelSettings.stakes;
-allProbs = levelSettings.probs;
-allAmbigs = levelSettings.ambigs;
-trialRepeats = levelSettings.repeats;
+allProbs = existingValueOrDefault(levelSettings, 'probs', []);
+allAmbigs = existingValueOrDefault(levelSettings, 'ambigs', []);
+trialRepeats = existingValueOrDefault(levelSettings, 'repeats', 1);
+
+if trialRepeats == 0
+  warning('As repeats in levelSettings were set to 0, no trials were generated.');
+  return
+end
+
+if isempty(allProbs) && isempty(allAmbigs)
+  warning(['As no available probability or ambiguity values were passed, no ' ...
+    'trials were generated.']);
+  return
+end
 
 % Generate all values for trials, so that they can be concatenated later
 [stakes_P, trialProbs] = ndgrid(allStakes, allProbs, 1:trialRepeats);
 [stakes_A, trialAmbigs] = ndgrid(allStakes, allAmbigs, 1:trialRepeats);
 % NOTE: The reason this works is that while `ndgrid` creates an n-dimensional
 %   matrix for each argument that has size > 1, all the values are extracted
-%   with `var(:)`, rendering dimensionality moot.
-
-% NOTE: Setting an empty matrix as either `stakes`, `probs` or `ambigs` will
-% result in `ndgrid` generating an empty matrix. In such case, issue a warning
-% and return control to the calling function.
-if isempty(trialProbs) || isempty(trialAmbigs)
-  warning(['One of the key fields in settings.game.levels has been set to '
-  'an empty matrix. This might be an intentional way of bypassing automatic '
-  'trial generation. If it is not, check your settings.']);
-  return;
-end
+%   with `var(:)`, rendering dimensionality moot. (It works with empty
+%   matrices.)
 
 stakes = [stakes_P(:); stakes_A(:)];
 probs  = [trialProbs(:); 0.5 * ones(size(trialAmbigs(:)))];
@@ -77,4 +83,16 @@ for k = 1:numel(getFields)
   % Add column to table
   trialTbl.(varName) = column;
 end
+end
+
+function val = existingValueOrDefault(structVar, fieldname, default)
+  if ~exist('default', 'var')
+    default = NaN;
+  end
+
+  if isfield(structVar, fieldname)
+    val = structVar.(fieldname);
+  else
+    val = default;
+  end
 end
