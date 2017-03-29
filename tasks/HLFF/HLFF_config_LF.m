7c196cd63a811258807400ab539b5636376eee60
Simon Podhajsky <simon.podhajsky@gmail.com>
2017-03-29 12:27:24 -0400

    Rename and reindent per-block configs
    (file rename coming in the next commit)

--- tasks/HLFF/HLFF_config_LF.m
+++ tasks/HLFF/HLFF_config_LF.m
@@ -1,22 +1,22 @@
-function s = HLFF_config_LF(initial_config)
+function c = HLFF_LFConfig(initial_config)
 % HLFF_CONFIG_LF Configure low-fat blocks of high-/low-fat food task
   if exist('initial_config', 'var')
-    s = initial_config;
+    c = initial_config;
   else
-    s = config();
+    c = config();
   end
 
-  s.runSetup.blockName = 'Food';
-  s.runSetup.conditions.payoffKind = 'Pretzels';
+  c.runSetup.blockName = 'Food';
+  c.runSetup.conditions.payoffKind = 'Pretzels';
 
-  s.objects.lottery.stakes.fontSize = 24;
-  s.objects.reference.fontSize = s.objects.lottery.stakes.fontSize;
+  c.objects.lottery.stakes.fontSize = 24;
+  c.objects.reference.fontSize = c.objects.lottery.stakes.fontSize;
 
   %% Lookup tables
-  s.runSetup.lookups.txt = {'No pretzels', '10 pretzels', '15 pretzels', ...
+  c.runSetup.lookups.txt = {'No pretzels', '10 pretzels', '15 pretzels', ...
     '20 pretzels', '30 pretzels'};
-  s.runSetup.lookups.img = {'nothing.png', 'pretzel1.jpeg', 'pretzel2.jpeg', ...
+  c.runSetup.lookups.img = {'nothing.png', 'pretzel1.jpeg', 'pretzel2.jpeg', ...
     'pretzel3.jpeg', 'pretzel4.jpeg'};
   % Fix images to path
-  s.runSetup.lookups.img = prependPath(s.runSetup.lookups.img, s.task.imgPath);
+  c.runSetup.lookups.img = prependPath(c.runSetup.lookups.img, c.task.imgPath);
 end
