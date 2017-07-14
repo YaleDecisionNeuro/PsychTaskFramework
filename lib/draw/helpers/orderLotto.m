7539af5f42d7d0e04e7221e488dbc7f56bda9765
Simon Podhajsky <simon.podhajsky@gmail.com>
2017-07-14 17:02:51 -0400

    Provide the actual fix to #115
    The coordinates were correct. The label assignment and placement was
    not.

--- lib/draw/helpers/orderLotto.m
+++ lib/draw/helpers/orderLotto.m
@@ -13,8 +13,7 @@ function [probOrder, amtOrder] = orderLotto(trialData)
   amtOrder = [winAmt, lossAmt];
 
   % NOTE: We could just go to Data.colors(trial) and compute reminder, & involve colors later
-  % FIXME: This seems like it should return colors wrong?
-  if trialData.colors == 1
+  if trialData.colors == 2
     probOrder = fliplr(probOrder);
     amtOrder = fliplr(amtOrder);
   end
