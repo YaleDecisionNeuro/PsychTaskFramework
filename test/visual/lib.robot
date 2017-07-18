YaleDecisionNeuro/PsychTaskFramework

commit 9c3757f841477ad83817110abf24753723d38b45

drawLotto: Add padding where necessary (#124)
In my testing, this doesn't look bad on either Windows legacy renderer
or the platform-independent renderer. The positioning also makes sense.

Mostly, what fixed the positioning problemwas reverting #122.

lib/draw/drawLotto.m
@@ -126,7 +126,7 @@ function drawPayoffImageAndLabel(payoffs)
 
   topLabelXY = [W/2 - topLabelDims(1)/2, Y1 - topLabelDims(2)/2] ...
     + offCenterByPx;
-  botLabelXY = [W/2 - botLabelDims(1)/2, Y3 + botLabelDims(2)] ...
+  botLabelXY = [W/2 - botLabelDims(1)/2, Y3 + botLabelDims(2) + padding] ...
     + offCenterByPx;
 
   payoffColor = blockConfig.draw.lottery.stakes.fontColor;
@@ -139,12 +139,13 @@ function drawPayoffImageAndLabel(payoffs)
   [ topImg, topImgDims ] = imgLookup(payoffs(1), imgLookupTbl, textureBank);
   [ botImg, botImgDims ] = imgLookup(payoffs(2), imgLookupTbl, textureBank);
 
-  bottomXY = [screenCenter - botImgDims(1)/2, Y3 + botLabelDims(2) + padding];
+  bottomXY = [screenCenter - botImgDims(1)/2, ...
+              Y3 + botLabelDims(2) + 2 * padding];
   bottomCoords = xyAndDimsToRect(bottomXY, botImgDims) + offCenterRect;
 
   % y-dim: Dodge the image from both lottery and the text, and leave some space
   topXY = [screenCenter - topImgDims(1)/2, ...
-           Y1 - topImgDims(2) - topLabelDims(2) - padding];
+           Y1 - topImgDims(2) - topLabelDims(2) - 2 * padding];
   topCoords = xyAndDimsToRect(topXY, topImgDims) + offCenterRect;
 
   Screen('DrawTexture', windowPtr, topImg, [], topCoords);
@@ -158,12 +159,11 @@ function drawPayoffAmount(payoffs)
   [ botPayoffString, botPayoffDims ] = dollarFormatter(payoffs(2), windowPtr);
 
   % 2. Calculate text position
-  padding = 20;
   topXY = [W/2 - topPayoffDims(1)/2, ...
-           Y1 - padding] + offCenterByPx;
+           Y1 - topPayoffDims(2)/2] + offCenterByPx;
   botXY = [W/2 - botPayoffDims(1)/2, ...
-           Y3 + botPayoffDims(2)] + offCenterByPx;
-
+           Y3 + botPayoffDims(2) + padding] + offCenterByPx;
+  
   % 3. Draw!
   payoffColor = blockConfig.draw.lottery.stakes.fontColor;
   DrawFormattedText(windowPtr, topPayoffString, topXY(1), topXY(2), payoffColor);

commit 8c2dc1dff3f7ab6c3b27f62ef9796b0ae91f9b0c

Refactor the testing logic into the common library
Tests the fix for regression in #124.

Generally streamlines the process of test creation - from now on, your
test should have the same name as the reference image. (cc: #63)

test/visual/correct-lotto-placement/correct-lotto-placement.robot
@@ -1,47 +1,24 @@
-*** Settings ***
+ *** Settings ***
 Documentation     PsychTaskFramework correct display of lottery
 Test Setup        Add Needed Images
 Library           SikuliLibrary
 Resource          ../lib.robot
+Test template     Display Correct Lottery With
 
 *** Variables ***
 ${IMAGE_DIR}      ${CURDIR}\\img
 
 *** Test Cases ***
-Display Correct Lottery
-    Sleep  1s
-    Focus Matlab Command Window
-    Input Text  ${EMPTY}  testRA_25
-    Press Special Key  ENTER
-    Wait Until Screen Contain   RA_monetary_USD10_25.png  30
-    Sleep  1s
-    Press Special Key  LEFT
-Display Medical Symbols
-    Sleep  1s
-    Focus Matlab Command Window
-    Input Text  ${EMPTY}  testMDM_img
-    Press Special Key  ENTER
-    Wait Until Screen Contain   MDM_amb50.png  30
-    Sleep  1s
-    Press Special Key  LEFT
-Display Cash Symbols
-    Sleep  1s
-    Focus Matlab Command Window
-    Input Text  ${EMPTY}  testRA_img
-    Press Special Key  ENTER
-    Wait Until Screen Contain   RA_moneyimg_ambig24.png  30
-    Sleep  1s
-    Press Special Key  LEFT
+Correct Win Probability And Color
+    testRA_25
+Correct Medical Image Display
+    testMDM_img
+Correct Monetary Image Display
+    testRA_img
+    testSODM_mon_img
+Correct Food Image Display
+    testHLFF_HF_0
 
 *** Keywords ***
 Add Needed Images
     Add Image Path    ${IMAGE_DIR}
-
-# This is duplicated from ../__init__.txt and should be put in a resource file
-# Focus Matlab Command Window
-#     Add Image Path    ${MATLAB_IMAGE_DIR}
-#     #Type With Modifiers  0  CTRL
-#     Wait Until Screen Contain  matlab-command-window-symbol.png  30
-#     Click  matlab-command-window-symbol.png
-#     Sleep  1s
-#     Press Special Key  ENTER

test/visual/lib.robot
@@ -14,3 +14,13 @@ Focus Matlab Command Window
     Click  matlab-command-window-symbol.png
     Sleep  1s
     Press Special Key  ENTER
+
+Display Correct Lottery With
+    [Arguments]   ${matlab_test_name}
+    Sleep   1s
+    Focus Matlab Command Window
+    Input Text   ${EMPTY}   ${matlab_test_name}
+    Press Special Key   ENTER
+    Wait Until Screen Contain   ${matlab_test_name}.png   15
+    Sleep   1s
+    Press Special Key   LEFT